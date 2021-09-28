f2229bb6b5270ada0832981c92f0a091cbd5c254
Rafael Castro <rafael.actt@gmail.com>
2021-09-28 13:51:22 -0300

    Instalacao do entity framework core e criação do contexto

---
+++ TesteAPI/TesteAPI.Repositorio/Contexto/CastGroupContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteAPI.Dominio.Entidades;
+
+/// <summary>
+/// Contexto para mapeamento das classes nas tabelas do banco
+/// </summary>
+namespace TesteAPI.Repositorio.Contexto
+{
+    public class CastGroupContexto:DbContext
+    {
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
