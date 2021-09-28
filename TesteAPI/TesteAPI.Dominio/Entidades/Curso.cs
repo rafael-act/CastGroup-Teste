a5fe373e58406a878e188ee2f6a351a2aa8cd446
Rafael Castro <rafael.actt@gmail.com>
2021-09-28 09:44:38 -0300

    Edição da classe curso no domínio

--- TesteAPI/TesteAPI.Dominio/Entidades/Curso.cs
+++ TesteAPI/TesteAPI.Dominio/Entidades/Curso.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TesteAPI.Dominio.Entidades
 {
-    public class Curso:Entidades
+    public class Curso : Entidades
     {
         public int CodigoCurso { get; set; }
         public string DescricaoAssunto { get; set; }
@@ -14,6 +14,7 @@ namespace TesteAPI.Dominio.Entidades
         public DateTime DataTernmino { get; set; }
         public int QuantidadeTurma { get; set; }
         public int CodigoCategoria { get; set; }
+        public Categoria Categoria { get; set; }
 
         public override void Validate()
         {
@@ -24,11 +25,14 @@ namespace TesteAPI.Dominio.Entidades
                 AdicionarCritica("Descrição do assunto não foi informado!");
             }
 
-            if (CodigoCategoria<1)
+            if (CodigoCategoria < 1)
             {
                 AdicionarCritica("Código da categoria informado é inválido!");
             }
-
+            if (DataInicio <= DateTime.Now)
+            {
+                AdicionarCritica("Não será permitida a inclusão de cursos com a data de início menor que a data atual!");
+            }
         }
     }
 }
